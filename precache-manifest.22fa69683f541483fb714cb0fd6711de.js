e9a3a506a7b788bee652a62b919329c80d677a5a
momzzze <momzzze@abv.bg>
2020-08-29 16:20:29 +0300

    s

---
+++ precache-manifest.22fa69683f541483fb714cb0fd6711de.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c43025c4e733793b35f3acf10bc40dce",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "4d9670d0added970bca8",
+    "url": "/monsters-rolodex/static/css/main.262d8d8e.chunk.css"
+  },
+  {
+    "revision": "91ef67d745446be22fd7",
+    "url": "/monsters-rolodex/static/js/2.865bbbd4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.865bbbd4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4d9670d0added970bca8",
+    "url": "/monsters-rolodex/static/js/main.839861d1.chunk.js"
+  },
+  {
+    "revision": "5d9871b43329dda10fc8",
+    "url": "/monsters-rolodex/static/js/runtime-main.98ead556.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/monsters-rolodex/static/media/logo.5d5d9eef.svg"
+  }
+]);
